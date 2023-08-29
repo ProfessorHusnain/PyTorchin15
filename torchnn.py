30f23db3314a89d8aa00f2698261db5b796f6341
ProfessorHusnain <mh571536@gmail.com>
2023-08-29 17:50:50 +0500

    Get Data

--- torchnn.py
+++ torchnn.py
@@ -7,4 +7,9 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+# Get data 
+train = datasets.MNIST(root="data", download=True, train=True, transform=ToTensor())
+dataset = DataLoader(train, 32)
+#1,28,28 - classes 0-9
+
  
